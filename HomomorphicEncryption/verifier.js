78b24d7dcc3f2d36c6e1ab3ea291cbe63f057337
Gabriel Stegmaier <gabriel.stegmaier@bluewin.ch>
2025-04-29 13:08:16 +0200

    renamed verifier_comp.js to verifier.js, removed original

--- HomomorphicEncryption/verifier.js
+++ HomomorphicEncryption/verifier.js
@@ -1,36 +1,16 @@
 const SEAL = require('node-seal');
+const tfhe_rs = require('../tfhe_comparison.node')
 const fs = require('fs');
 const { subtle } = globalThis.crypto;
 
 // function to generate encryption keys for HE
 async function generateEncryptionKeys() {
-    const seal = await SEAL();
-    const schemeType = seal.SchemeType.ckks;
-    const securityLevel = seal.SecurityLevel.tc128;
-    const polyModulusDegree = 8192;
-    const bitSizes = [60,20,20,20,20,60];
-    // const bitSizeFloat = 40;
-
-    const parms = seal.EncryptionParameters(schemeType);
-    parms.setPolyModulusDegree(polyModulusDegree);
-    parms.setCoeffModulus(seal.CoeffModulus.Create(polyModulusDegree, Int32Array.from(bitSizes)));
-
-    const context = seal.Context(parms, true, securityLevel);
-    if (!context.parametersSet()) {
-        throw new Error('Could not set the parameters in the given context. Please try different encryption parameters.');
-    }
-
-    const keyGenerator = seal.KeyGenerator(context);
-    const secretKey = keyGenerator.secretKey();
-    const publicKey = keyGenerator.createPublicKey();
-
-    const encoder = seal.CKKSEncoder(context);
-    const encryptor = seal.Encryptor(context, publicKey);
-    const evaluator = seal.Evaluator(context);
-    const decryptor = seal.Decryptor(context, secretKey);
+    const keys = tfhe_rs.getKeys();
+    const decryptor = keys[0];
+    const evaluator = keys[1];
+    const encryptor = keys[2];
 
     const instances = {
-        encoder: encoder,
         encryptor: encryptor,
         decryptor: decryptor,
         evaluator: evaluator,
@@ -59,21 +39,18 @@ async function verifierSign(key, data) {
     return signature;
 }
 
-async function verifierEncodeEncrypt(value, encoder, encryptor) {
+async function verifierEncrypt(value, encryptor) {
     // const seal = await SEAL();
-    const bitSizeFloat = 40;
-    const plain = encoder.encode(Float64Array.from([value]), Math.pow(2, bitSizeFloat));
-    const cipher = encryptor.encrypt(plain);
-    console.log('size cipher', Buffer.byteLength(JSON.stringify(cipher.save())))
+    const pValue = parseInt(value);
+    const cipher = tfhe_rs.encryptPublicKey(pValue, encryptor);
+    //console.log('size cipher', Buffer.byteLength(JSON.stringify(cipher.save())))
     return cipher;
 }
 
-async function verifierDecryptDecode(value, encoder, decryptor) {
-    const pResult = decryptor.decrypt(value);
-    const resultStudent = encoder.decode(pResult);
-    console.log("\tDecoded Result:", resultStudent[0]);
-    const result = parseFloat(resultStudent[0]);
-    if (result <= 0) {
+async function verifierDecrypt(value, decryptor) {
+    const resultStudent = tfhe_rs.decrypt(value, decryptor);
+    console.log("\tDecoded Result:", resultStudent);
+    if (!resultStudent) {
         console.log("\tVALID Issuance Date");
         return true;
     } else {
@@ -87,31 +64,29 @@ async function verifierSetUp(timestamp) {
     let instances = await generateEncryptionKeys();
     let signingKeys = await generateSignatureKeys();
 
-    let encoder   = instances.encoder;
     let encryptor = instances.encryptor;
     let decryptor = instances.decryptor;
-    let evaluator   = instances.evaluator;
+    let evaluator  = instances.evaluator;
 
     let signPublicKey = signingKeys.publicKey;
     let signPrivateKey = signingKeys.privateKey;
 
-    thresholdCiphertext = await verifierEncodeEncrypt(timestamp, encoder, encryptor);
+    thresholdCiphertext = await verifierEncrypt(timestamp, encryptor);
     signature = await verifierSign(signPrivateKey, thresholdCiphertext);
 
     return {
         signPublicKey: signPublicKey,
         verifierSignature: signature,
         thresholdCiphertext: thresholdCiphertext,
-        verifierEncoder: encoder,
         verifierEncryptor: encryptor,
         verifierDecryptor: decryptor,
         proverEvaluator: evaluator,
     }
 }
 
-async function verifierProve(proverVesult, encoder, decryptor) {
-    let result = await verifierDecryptDecode(proverVesult, encoder, decryptor);
+async function verifierProve(proverVesult, decryptor) {
+    let result = await verifierDecrypt(proverVesult, decryptor);
     return result;
 }
 
-module.exports = { verifierSetUp, verifierProve, generateEncryptionKeys, generateSignatureKeys, verifierEncodeEncrypt, verifierDecryptDecode, verifierSign };
+module.exports = { verifierSetUp, verifierProve, generateEncryptionKeys, generateSignatureKeys, verifierEncrypt, verifierDecrypt, verifierSign };
